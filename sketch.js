a028406fc653c6ee69f1d9cfaf7db21075edd27e
Arthur dos santos de jesus <141414181+arthurstts@users.noreply.github.com>
2023-08-14 12:03:09 -0300

    Add files via upload
    Como chegar até mim

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(550, 550);
+   background("silver");
+}
+
+function draw() {
+ 
+  stroke("black")
+  fill("gold");
+  
+ if(mouseIsPressed){rect(mouseX,mouseY,50,50)
+   
+ }
+  
+  
+  
+}
